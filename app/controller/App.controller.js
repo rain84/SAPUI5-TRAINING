17e87ef90a7f6fadd234fed70bd54760b914bd3f
rain84 <rainy_sky@mail.ru>
2017-07-15 20:40:18 +0300

    Step 8 implemented

--- app/controller/App.controller.js
+++ app/controller/App.controller.js
@@ -2,18 +2,38 @@ sap.ui.define([
   'sap/ui/core/mvc/Controller',
   'sap/m/MessageToast',
   'sap/ui/model/json/JSONModel',
+  "sap/ui/model/resource/ResourceModel"
   ],
 
-  (Controller, MessageToast, JSONModel) => Controller.extend('app.controller.App', {
-    onInit() {
-      const oData = {recipient: {name: 'World'}};
-      const oModel = new JSONModel(oData);
+  (Controller, MessageToast, JSONModel, ResourceModel) =>
+    Controller.extend('app.controller.App', {
+      onInit() {
+        const data = {recipient: {name: 'World'}};
+        const model = new JSONModel(data);
+        const i18nModel = new ResourceModel({
+          bundleName: "app.i18n.i18n"
+        });
 
-      this.getView().setModel(oModel);
-    },
+        this.getView().setModel(i18nModel, "i18n");
+        this.getView().setModel(model);
+      },
 
-    onShowHello() {
-      MessageToast.show('Hello world!');
-    }
+      onShowHello() {
+        var recipient = this
+          .getView()
+          .getModel()
+          .getProperty("/recipient/name")
+          ;
+
+        var bundle = this
+          .getView()
+          .getModel("i18n")
+          .getResourceBundle()
+          ;
+
+        var msg = bundle.getText("helloMsg", [recipient]);
+
+        MessageToast.show(msg);
+      }
   })
 );
